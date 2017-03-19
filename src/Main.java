65b1ecd11507e3da8e3bba4e68018bcaa358a917
pkrowicki <pkrowicki@outlook.com>
2017-03-19 01:04:51 +0100

    Methods

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@
 public class Main {
 
     private static void L3PrimitiveDT() {
-        byte var1 = Byte.MAX_VALUE;
+        byte var1 = Byte.MAX_VALUE;  // var1 = Byte.MAX_VALUE is an expression, whole statement is... statement.
         short var2 = Short.MAX_VALUE;
         int var3 = Integer.MAX_VALUE;
         long var4 = Long.MAX_VALUE;
@@ -21,13 +21,55 @@ public class Main {
         char var7 = '\u00a9'; // 2 bytes
         boolean var8 = false;
         String var9 = "Max values of Java Primitives";
-        var9 = var9+",\nand this sentence is 9th, called String.\n\n";
+        var9 = var9+",\nand this sentence is 9th (not PDT, but...), called String.\n\n";
 
         System.out.println(var9 +"Byte: " + var1 + "\nShort: " + var2 + "\nInteger: " + var3 + "\nLong: " + var4 + "\nFloat: " + var5 + "\nDouble: " + var6 + "\nChar: " + var7 +"\nBoolean: " + var8);
+    }
+
+    private static void L4Of1(int liczba){
+        System.out.println(liczba);
+    }
+
+    private static void L4Operators() {
+
+        int result = 5;
+        L4Of1(result+=27);
+        L4Of1(result*=6);
+        L4Of1(result-=11);
+        L4Of1(result/=3);
+        L4Of1(--result);
+        L4Of1(++result);
+        boolean isToF = true;
+        String trojoperator = isToF ? "Wartosc to true" : "Wartosc to false";
+        System.out.println(trojoperator);
+
+    }
+
+    private static int L5MethodsExmpleAndOverloading(boolean isGameOver, int score, int levelNumber, int bonusPnts){
+        if (isGameOver){
+            return score+(levelNumber*bonusPnts);
+        }
+        else{
+            return 0;
+        }
+    }
+    private static int L5MethodsExampleAndOverloading(int powerScore){
+        return powerScore*10;
     }
 
     public static void main(String[] args) {
 
         L3PrimitiveDT();
+        L4Operators();
+        //L5 bracket
+        System.out.println(L5MethodsExmpleAndOverloading(true,1232, 3, 200));
+        boolean gameEndVarBool = true;
+        int levelNmbr, bonus, highscore = 1398;
+        int finalscore = L5MethodsExmpleAndOverloading(gameEndVarBool, highscore, levelNmbr=5,bonus=450);
+        System.out.println(finalscore);
+        //Overloading method:
+        finalscore=L5MethodsExampleAndOverloading(200);
+        System.out.println(finalscore);
+        //End of L5 bracket
     }
 }
